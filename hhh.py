1a75565825950fa1fa1d42c995940af909d40380
Gagrio <95764058+Tomass-Nedveds@users.noreply.github.com>
2023-12-06 17:48:55 +0200

    Update hhh.py

--- hhh.py
+++ hhh.py
@@ -130,14 +130,14 @@ class Ammo(pygame.sprite.Sprite):
             self.kill()  # убиваем
 
 # наша игровая графика загрузка картинок
-backgrounds = pygame.image.load("Image/background.png").convert_alpha()
+backgrounds = pygame.image.load("background.png").convert_alpha()
 back_rect = backgrounds.get_rect()
-playerimg = pygame.image.load("Image/player.png").convert_alpha()
-buletImg = pygame.image.load("Image/Bulet.png").convert_alpha()  # вариант создать новый список и пройтись в цикле по этому списку перебирая другие
-rockImg = pygame.image.load("Image/Rock2.png").convert_alpha()  # вариаты картинок например для врагов или пулек
+playerimg = pygame.image.load("player.png").convert_alpha()
+buletImg = pygame.image.load("Bulet.png").convert_alpha()  # вариант создать новый список и пройтись в цикле по этому списку перебирая другие
+rockImg = pygame.image.load("Rock2.png").convert_alpha()  # вариаты картинок например для врагов или пулек
 pygame.mixer.music.set_volume(0.3)
 
-pygame.mixer.music.load("Audio/Shut.wav")
+pygame.mixer.music.load("Shut.wav")
 
 all_sprites = pygame.sprite.Group()  # Добовление спрайта в групу
 enemy = pygame.sprite.Group()
